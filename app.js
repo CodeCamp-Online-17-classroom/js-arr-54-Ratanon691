be4296089bf741f75168f92f0597cbe7af9a0c31
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-14 09:38:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+let names = ['Jack', 'John', 'Bill', 'John', 'Rick', 'Bill', 'John'];
+let nameCounts = {}
+
+names.forEach(name => {
+  if (nameCounts[name]) {
+    nameCounts[name]++
+  } else {
+    nameCounts[name] = 1
+  }
+})
+
+console.log(nameCounts) // Output: {Jack: 1, John: 3, Bill: 2, Rick: 1}
